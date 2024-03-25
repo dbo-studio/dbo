e90aa5e06ebcec4f3412b1ca9595940bf5586a4a
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-03-25 15:18:17 +0330

    disable end container tabs

--- src/components/layout/MainContainer/EndContainer.tsx
+++ src/components/layout/MainContainer/EndContainer.tsx
@@ -1,52 +1,50 @@
 import DBFelids from '@/src/components/common/DBFelids/DBFelids';
-import locales from '@/src/locales';
 import { useSettingStore } from '@/src/store/settingStore/setting.store';
-import { Box, Tab, Tabs } from '@mui/material';
-import React, { useMemo, useState } from 'react';
+import { Box } from '@mui/material';
 import ResizableXBox from '../../base/ResizableBox/ResizableXBox';
 import { EndContainerStyled } from './Container.styled';
 
-const tabs = [
-  {
-    id: 0,
-    name: locales.tables,
-    content: (
-      <>
-        <DBFelids />
-      </>
-    )
-  },
-  {
-    id: 1,
-    name: locales.ddl,
-    content: (
-      <>
-        <p>DDL</p>
-      </>
-    )
-  },
-  {
-    id: 2,
-    name: locales.info,
-    content: (
-      <>
-        <p>Info</p>
-      </>
-    )
-  }
-];
+// const tabs = [
+//   {
+//     id: 0,
+//     name: locales.tables,
+//     content: (
+//       <>
+//         <DBFelids />
+//       </>
+//     )
+//   }
+//   {
+//     id: 1,
+//     name: locales.ddl,
+//     content: (
+//       <>
+//         <p>DDL</p>
+//       </>
+//     )
+//   },
+//   {
+//     id: 2,
+//     name: locales.info,
+//     content: (
+//       <>
+//         <p>Info</p>
+//       </>
+//     )
+//   }
+// ];
 
 export default function EndContainer() {
-  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
+  // const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
   const { sidebar, updateSidebar } = useSettingStore();
 
-  const selectedTabContent = useMemo(() => {
-    return tabs.find((obj) => obj.id === selectedTabId)?.content;
-  }, [selectedTabId]);
+  // const selectedTabContent = useMemo(() => {
+  //   return tabs.find((obj) => obj.id === selectedTabId)?.content;
+  // }, [selectedTabId]);
 
-  const onSelectedTabChanged = (event: React.SyntheticEvent, id: number) => {
-    setSelectedTabId(id);
-  };
+  // const onSelectedTabChanged = (event: React.SyntheticEvent, id: number) => {
+  //   setSelectedTabId(id);
+  // };
 
   return (
     <ResizableXBox
@@ -56,13 +54,16 @@ export default function EndContainer() {
       maxWidth={500}
     >
       <EndContainerStyled>
-        <Tabs value={selectedTabId} onChange={onSelectedTabChanged}>
+        {/* <Tabs value={selectedTabId} onChange={onSelectedTabChanged}>
           <Tab label='Fields' />
           <Tab label='DDL' />
           <Tab label='Info' />
-        </Tabs>
+        </Tabs> */}
 
-        <Box role='tabpanel'>{selectedTabContent}</Box>
+        {/* <Box role='tabpanel'>{selectedTabContent}</Box> */}
+        <Box role='tabpanel'>
+          <DBFelids />
+        </Box>
       </EndContainerStyled>
     </ResizableXBox>
   );
