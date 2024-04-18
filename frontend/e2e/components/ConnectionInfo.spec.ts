e3521b01b0f3a3718afd0d9f1be896b073627c59
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-04-18 17:12:36 +0330

    remove useless files and update nextjs version

--- frontend/e2e/components/ConnectionInfo.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test.describe('ConnectionInfo', function () {
-  test('should see connection info in app header', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByText('PostgreSQL 15.1: public: orders: SQL Query')).toBeVisible();
-  });
-});
