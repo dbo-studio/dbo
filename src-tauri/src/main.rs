7e850a7e9cdb1ad9c0325f15c53e42627260fce5
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-07-01 15:41:43 +0330

    feat: add new icons feat: add window state saver

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     run_server();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![get_backend_host])
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
