267b50f0d08bbffc22734a97d6b19d5c3afe80fb
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-07-08 00:50:27 +0330

    fixing build workflow

--- docs/scripts/build_init.sh
+++ docs/scripts/build_init.sh
@@ -108,4 +108,5 @@ get_platform_name() {
 build_backend(){
     cd "${BACKEND_DIR}" && go mod download
     CGO_ENABLED=1 go build -p=8 --tags "release" -ldflags "-w" -o "${DESKTOP_DIR}/bin/dbo-bin-$(get_platform_name)" "${BACKEND_DIR}"/*.go
+    echo build dbo-bin-$(get_platform_name)
 }
