d7111286ac5137a6e884a39e5e204a2cabedbc86
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-08-19 14:28:39 +0330

    style: fix style of scrollbar styles

---
+++ frontend/src/components/shared/DBDataGrid/DataGridWrapper.styled.ts
@@ -0,0 +1,8 @@
+import { Box, styled } from '@mui/material';
+
+export const DataGridWrapperStyled = styled(Box)(() => ({
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1
+}));
