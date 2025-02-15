338822981602ef136e4371bdac08133cdf9461f8
mobin yazdanparast <mobin.y@780.team>
2025-02-15 19:30:30 +0330

    run format

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_PUBLIC_SERVER_URL: string;
-	readonly VITE_VERSION: string;
+  readonly VITE_PUBLIC_SERVER_URL: string;
+  readonly VITE_VERSION: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
