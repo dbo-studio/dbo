93330072374cbc189423444de842beccd0931b7c
mobin yazdanparast <mobin.y@780.team>
2025-02-15 19:30:10 +0330

    fix: version not work in desktop builds

--- frontend/src/components/common/Settings/AboutPanel/AboutPanel.tsx
+++ frontend/src/components/common/Settings/AboutPanel/AboutPanel.tsx
@@ -1,57 +1,82 @@
-import CustomIcon from '@/components/base/CustomIcon/CustomIcon';
-import { constants } from '@/core/constants';
-import locales from '@/locales';
-import { Box, Typography, useTheme } from '@mui/material';
-import { useEffect, useState } from 'react';
+import CustomIcon from "@/components/base/CustomIcon/CustomIcon";
+import { constants } from "@/core/constants";
+import locales from "@/locales";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function AboutPanel() {
-  const theme = useTheme();
-  const [about, setAbout] = useState({
-    version: null
-  });
+	const theme = useTheme();
+	const [about, setAbout] = useState<{
+		version: string | null;
+	}>({
+		version: null,
+	});
 
-  useEffect(() => {
-    setAbout({
-      version: import.meta.env.VITE_VERSION
-    });
-  }, []);
+	useEffect(() => {
+		setAbout({
+			version: import.meta.env.VITE_VERSION,
+		});
+	}, []);
 
-  return (
-    <Box>
-      <Box mt={theme.spacing(2)} textAlign={'center'}>
-        <img src='/app-icon/icon-512.png' alt='logo' height={100} width={100} />
-      </Box>
+	return (
+		<Box>
+			<Box mt={theme.spacing(2)} textAlign={"center"}>
+				<img src="/app-icon/icon-512.png" alt="logo" height={100} width={100} />
+			</Box>
 
-      {about.version && (
-        <Box mt={theme.spacing(2)} textAlign={'center'}>
-          <Typography variant='caption'>{locales.version}</Typography>
-          <Typography variant='subtitle2'>{about.version}</Typography>
-        </Box>
-      )}
+			{about.version && (
+				<Box mt={theme.spacing(2)} textAlign={"center"}>
+					<Typography variant="caption">{locales.version}</Typography>
+					<Typography variant="subtitle2">{about.version}</Typography>
+				</Box>
+			)}
 
-      <Box mt={theme.spacing(2)} display={'flex'} alignItems={'center'} flexDirection={'column'}>
-        <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
-          <a href={constants.releasesUrl} target='_blank' rel='noreferrer' style={{ textDecoration: 'none' }}>
-            <Typography style={{ marginRight: theme.spacing(1) }} variant='caption'>
-              {locales.releases_url}
-            </Typography>
-            <CustomIcon type='externalLink' size='xs' />
-          </a>
-        </Box>
-        <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
-          <a
-            href={constants.reportIssueUrl}
-            target='_blank'
-            rel='noreferrer'
-            style={{ textDecoration: 'none', marginRight: theme.spacing(1) }}
-          >
-            <Typography style={{ marginRight: theme.spacing(1) }} variant='caption'>
-              {locales.report_an_issue}
-            </Typography>
-            <CustomIcon type='externalLink' size='xs' />
-          </a>
-        </Box>
-      </Box>
-    </Box>
-  );
+			<Box
+				mt={theme.spacing(2)}
+				display={"flex"}
+				alignItems={"center"}
+				flexDirection={"column"}
+			>
+				<Box display={"flex"} flexDirection={"row"} alignItems={"center"}>
+					<a
+						href={constants.releasesUrl}
+						target="_blank"
+						rel="noreferrer"
+						style={{
+							textDecoration: "none",
+							color: theme.palette.text.primary,
+						}}
+					>
+						<Typography
+							style={{ marginRight: theme.spacing(1) }}
+							variant="caption"
+						>
+							{locales.releases_url}
+						</Typography>
+						<CustomIcon type="externalLink" size="xs" />
+					</a>
+				</Box>
+				<Box display={"flex"} flexDirection={"row"} alignItems={"center"}>
+					<a
+						href={constants.reportIssueUrl}
+						target="_blank"
+						rel="noreferrer"
+						style={{
+							textDecoration: "none",
+							marginRight: theme.spacing(1),
+							color: theme.palette.text.primary,
+						}}
+					>
+						<Typography
+							style={{ marginRight: theme.spacing(1) }}
+							variant="caption"
+						>
+							{locales.report_an_issue}
+						</Typography>
+						<CustomIcon type="externalLink" size="xs" />
+					</a>
+				</Box>
+			</Box>
+		</Box>
+	);
 }
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_PUBLIC_SERVER_URL: string;
+	readonly VITE_VERSION: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
