eea3a873f1e6a3548c842555650ee293e7674da9
mobin yazdanparast <mobin.y@780.team>
2025-07-15 11:06:40 +0330

    fix: tabs removed after change connection

--- frontend/src/store/tabStore/tab.store.ts
+++ frontend/src/store/tabStore/tab.store.ts
@@ -1,6 +1,6 @@
+import type { TabType } from '@/types';
 import { create, type StoreApi, type UseBoundStore } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
-import type { TabType } from '@/types';
 import { useConnectionStore } from '../connectionStore/connection.store';
 import { createTabColumnSlice } from './slices/tabColumn.slice';
 import { createTabFilterSlice } from './slices/tabFilter.slice';
@@ -50,15 +50,7 @@ export const useTabStore: UseBoundStore<StoreApi<TabState>> = create<TabState>()
           return get().tabs.find((t) => t.connectionId === currentConnectionId && t.id === get().selectedTabId);
         },
         updateTabs: (newTabs: TabType[]): void => {
-          const currentConnectionId = useConnectionStore.getState().currentConnectionId;
-          if (!currentConnectionId) {
-            return;
-          }
-
-          const tabs = get().tabs.filter((t) => t.connectionId !== currentConnectionId);
-          tabs.push(...newTabs);
-
-          set({ tabs });
+          set({ tabs: newTabs });
         },
         updateSelectedTab: (newSelectedTab: TabType | undefined): void => {
           if (newSelectedTab === undefined) {
@@ -66,18 +58,16 @@ export const useTabStore: UseBoundStore<StoreApi<TabState>> = create<TabState>()
             return;
           }
 
-          const tabs = get()
-            .getTabs()
-            .map((t: TabType) => {
-              if (
-                t.id === newSelectedTab.id &&
-                t.mode === newSelectedTab.mode &&
-                t.connectionId === newSelectedTab.connectionId
-              ) {
-                return newSelectedTab;
-              }
-              return t;
-            });
+          const tabs = get().tabs.map((t: TabType) => {
+            if (
+              t.id === newSelectedTab.id &&
+              t.mode === newSelectedTab.mode &&
+              t.connectionId === newSelectedTab.connectionId
+            ) {
+              return newSelectedTab;
+            }
+            return t;
+          });
 
           set({ tabs, selectedTabId: newSelectedTab.id });
         },
