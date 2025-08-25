6004166f5b81ac741162e3c39dc271f9bca17749
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2025-08-25 19:15:53 +0330

    add switch connection test

--- e2e/tests/connections.spec.ts
+++ e2e/tests/connections.spec.ts
@@ -1,17 +1,14 @@
 import { expect, test } from '@playwright/test';
 
+test('Connections', async ({page}) => {
+    await page.goto('/');
+    
+    await page.waitForLoadState('networkidle');
+    
+    await page.waitForTimeout(1000);
 
-test('Connections', async ({ page }) => {
-
-    test.step('Create a new connection flow', async () => {
-        await page.goto('/');
-
-        const addConnectionModal = page.getByText('New connection');
-        if (await addConnectionModal.isVisible()) {
-            await addConnectionModal.click();
-            await expect(page.getByText('New connection')).toBeVisible();
-        }
-
+    await test.step('Create a new connection', async () => {
+        await expect(page.getByText('New connection')).toBeVisible();
 
         await page.getByTestId('selected-connection-PostgreSQL').click();
         await page.getByTestId('select-connection').click();
@@ -22,24 +19,69 @@ test('Connections', async ({ page }) => {
         await page.locator('input[name="username"]').fill('default');
         await page.locator('input[name="password"]').fill('secret');
 
+        const testConnectionPromise = page.waitForResponse(response => 
+            response.url().includes('connections/ping') && response.status() === 200,
+            { timeout: 10000 }
+        );
+        
         await page.getByTestId('test-connection').click();
-        await page.waitForResponse(response => response.url().includes('connections/ping') && response.status() === 200);
+        await testConnectionPromise;
 
+        const createConnectionPromise = page.waitForResponse(response => 
+            response.url().includes('connections') && response.status() === 200,
+            { timeout: 10000 }
+        );
+        
         await page.getByTestId('create-connection').click();
+        await createConnectionPromise;
 
-        await expect(addConnectionModal).toBeHidden();
+        await expect(page.getByText('New connection')).toBeHidden();
         await page.waitForTimeout(1000);
         await expect(page.getByTestId('connection-item-local')).toBeVisible();
     });
-   
 
-    test.step('Create a new connection flow', async () => {
-        await page.goto('/');
+    await test.step('Create another connection', async () => {
+        await page.waitForTimeout(1000);
+        
+        await page.waitForLoadState('networkidle');
 
-        const addConnectionModal = page.getByText('New connection');
-        if (await addConnectionModal.isVisible()) {
-            await addConnectionModal.click();
-            await expect(page.getByText('New connection')).toBeVisible();
-        }
+        await page.getByTestId('add-connection').click();
+        await expect(page.getByText('New connection')).toBeVisible();
+
+        await page.getByTestId('selected-connection-PostgreSQL').click();
+        await page.getByTestId('select-connection').click();
+
+        await page.locator('input[name="name"]').fill('local2');
+        await page.locator('input[name="host"]').fill('sample-pgsql');
+        await page.locator('input[name="port"]').fill('5432');
+        await page.locator('input[name="username"]').fill('default');
+        await page.locator('input[name="password"]').fill('secret');
+
+        const testConnectionPromise = page.waitForResponse(response => 
+            response.url().includes('connections/ping') && response.status() === 200,
+            { timeout: 10000 }
+        );
+        
+        await page.getByTestId('test-connection').click();
+        await testConnectionPromise;
+
+        const createConnectionPromise = page.waitForResponse(response => 
+            response.url().includes('connections') && response.status() === 200,
+            { timeout: 10000 }
+        );
+        
+        await page.getByTestId('create-connection').click();
+        await createConnectionPromise;
+
+        await expect(page.getByText('New connection')).toBeHidden();
+        await page.waitForTimeout(1000);
+        await expect(page.getByTestId('connection-item-local2')).toBeVisible();
+    });
+
+    test.step('Switch to the second connection', async () => {
+        await page.getByTestId('connection-item-local2').click();
+        await expect(page.getByTestId('connection-item-local2')).toBeVisible();
+
+       await expect(page.getByRole('heading', { name: 'local2 | postgresql 16.1 : SQL' })).toBeVisible();
     });
 });
--- e2e/utils/global.setup.ts
+++ e2e/utils/global.setup.ts
@@ -2,19 +2,19 @@ import { type FullConfig } from '@playwright/test';
 import { exec } from 'child_process';
 
 async function globalSetup(config: FullConfig) {
-    // exec('rm -rf ./../backend/data/dbo.db_testing.db', (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`error: ${error.message}`);
-    //         return;
-    //     }
-    // });
+    exec('rm -rf ./../backend/data/dbo.db_testing.db', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+    });
 
-    // exec('docker restart dbo-studio-dev-api', (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.error(`error: ${error.message}`);
-    //         return;
-    //     }
-    // });
+    exec('docker restart dbo-studio-dev-api', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+    });
 }
 
 export default globalSetup;
