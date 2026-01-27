754b928accf2e59a5b3f689b33bb874ec87a8264
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2026-01-28 00:20:59 +0330

    fix: use local loader for monaco editor instead of cdn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,14 +9,17 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { loader } from '@monaco-editor/react';
 import { CssBaseline, GlobalStyles, type Interpolation, type Theme } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as monaco from 'monaco-editor';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'sonner';
-
 import ThemeProvider from './core/theme/index.tsx';
 import Home from './routes/index.tsx';
+
+loader.config({ monaco });
 
 const queryClient = new QueryClient({
   defaultOptions: {
