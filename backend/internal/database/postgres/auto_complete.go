606c11dba313a0d9f618c29026120e290369590d
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2026-03-19 16:27:05 +0330

    feat: return all column to editor autocomplete if user doesn't select any schema

--- backend/internal/database/postgres/auto_complete.go
+++ backend/internal/database/postgres/auto_complete.go
@@ -62,37 +62,35 @@ func (r *PostgresRepository) AutoComplete(ctx context.Context, data *dto.AutoCom
 
 	columns := make(map[string][]string)
 
-	if data.Schema != nil {
-		gColumns, gColumnsCtx := errgroup.WithContext(ctx)
-		var columnMap sync.Map
+	gColumns, gColumnsCtx := errgroup.WithContext(ctx)
+	var columnMap sync.Map
 
-		for _, table := range tables {
-			tableName := table.Name
-			gColumns.Go(func() error {
-				columnResult, err := r.columns(gColumnsCtx, &tableName, data.Schema, nil, false, true)
-				if err != nil {
-					return err
-				}
-				columnMap.Store(tableName, lo.Map(columnResult, func(x Column, _ int) string { return x.ColumnName }))
-				return nil
-			})
-		}
-
-		if err := gColumns.Wait(); err != nil {
-			return nil, err
-		}
-
-		columnMap.Range(func(key, value any) bool {
-			tableName, ok := key.(string)
-			if !ok {
-				return true
+	for _, table := range tables {
+		tableName := table.Name
+		gColumns.Go(func() error {
+			columnResult, err := r.columns(gColumnsCtx, &tableName, data.Schema, nil, false, true)
+			if err != nil {
+				return err
 			}
-			if columnList, ok := value.([]string); ok {
-				columns[tableName] = columnList
-			}
-			return true
+			columnMap.Store(tableName, lo.Map(columnResult, func(x Column, _ int) string { return x.ColumnName }))
+			return nil
 		})
 	}
+
+	if err := gColumns.Wait(); err != nil {
+		return nil, err
+	}
+
+	columnMap.Range(func(key, value any) bool {
+		tableName, ok := key.(string)
+		if !ok {
+			return true
+		}
+		if columnList, ok := value.([]string); ok {
+			columns[tableName] = columnList
+		}
+		return true
+	})
 
 	return &dto.AutoCompleteResponse{
 		Databases: lo.Map(databases, func(x Database, _ int) string { return x.Name }),
