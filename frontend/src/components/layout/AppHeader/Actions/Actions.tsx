7d46f6f2d68c6ce9edeea41000b6243778a2c9c2
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2026-03-21 17:42:28 +0330

    feat: change left sidebar toggle button location

--- frontend/src/components/layout/AppHeader/Actions/Actions.tsx
+++ frontend/src/components/layout/AppHeader/Actions/Actions.tsx
@@ -1,41 +1,46 @@
+import CustomIcon from '@/components/base/CustomIcon/CustomIcon';
 import { constants } from '@/core/constants';
-import { useSettingStore } from '@/store/settingStore/setting.store.ts';
-import { IconButton, Stack, Tooltip } from '@mui/material';
-import type { JSX } from 'react';
-import CustomIcon from '../../../base/CustomIcon/CustomIcon.tsx';
 import locales from '@/locales/index.ts';
+import { useSettingStore } from '@/store/settingStore/setting.store.ts';
+import { Badge, IconButton, Stack, Tooltip } from '@mui/material';
+import type { JSX } from 'react';
 
 export default function Actions(): JSX.Element {
   const updateUI = useSettingStore((state) => state.updateUI);
+  const release = useSettingStore((state) => state.general.release);
 
-  const handelUpdateSidebar = (direction: 'right' | 'left'): void => {
+  const handelUpdateSidebar = (): void => {
     const sidebar = useSettingStore.getState().ui.sidebar;
-    if (direction === 'right') {
-      if (!sidebar.showRight) {
-        updateUI({ sidebar: { ...sidebar, showRight: !sidebar.showRight, rightWidth: constants.defaultSidebarWidth } });
-      } else {
-        updateUI({ sidebar: { ...sidebar, showRight: !sidebar.showRight } });
-      }
+    if (!sidebar.showRight) {
+      updateUI({ sidebar: { ...sidebar, showRight: !sidebar.showRight, rightWidth: constants.defaultSidebarWidth } });
+    } else {
+      updateUI({ sidebar: { ...sidebar, showRight: !sidebar.showRight } });
     }
+  };
 
-    if (direction === 'left') {
-      if (!sidebar.showLeft) {
-        updateUI({ sidebar: { ...sidebar, showLeft: !sidebar.showLeft, leftWidth: constants.defaultSidebarWidth } });
-      } else {
-        updateUI({ sidebar: { ...sidebar, showLeft: !sidebar.showLeft } });
-      }
-    }
+  const openSettings = (): void => {
+    useSettingStore.getState().updateUI({ showSettings: { open: true, tab: 0 } });
   };
 
   return (
     <Stack direction='row' justifyContent='flex-end'>
-      <Tooltip title={locales.left_sidebar}>
-        <IconButton aria-label='sideLeft' onClick={(): void => handelUpdateSidebar('left')}>
-          <CustomIcon type={'sideLeft'} size={'m'} />
-        </IconButton>
-      </Tooltip>
+      {release ? (
+        <Tooltip title={locales.new_version_available}>
+          <IconButton aria-label='settings' onClick={openSettings}>
+            <Badge variant='dot' color='warning'>
+              <CustomIcon type={'settings'} size={'m'} />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title={locales.settings}>
+          <IconButton aria-label='settings' onClick={openSettings}>
+            <CustomIcon type={'settings'} size={'m'} />
+          </IconButton>
+        </Tooltip>
+      )}
       <Tooltip title={locales.right_sidebar}>
-        <IconButton aria-label='sideRight' onClick={(): void => handelUpdateSidebar('right')}>
+        <IconButton aria-label='sideRight' onClick={handelUpdateSidebar}>
           <CustomIcon type={'sideRight'} size={'m'} />
         </IconButton>
       </Tooltip>
--- frontend/src/components/layout/AppHeader/Leading/Leading.tsx
+++ frontend/src/components/layout/AppHeader/Leading/Leading.tsx
@@ -1,33 +1,29 @@
 import CustomIcon from '@/components/base/CustomIcon/CustomIcon.tsx';
+import { constants } from '@/core/constants';
 import locales from '@/locales';
 import { useSettingStore } from '@/store/settingStore/setting.store';
-import { Badge, IconButton, Stack, Tooltip } from '@mui/material';
+import { IconButton, Stack, Tooltip } from '@mui/material';
 import type { JSX } from 'react';
 
 export default function Leading(): JSX.Element {
-  const release = useSettingStore((state) => state.general.release);
+  const updateUI = useSettingStore((state) => state.updateUI);
 
-  const openSettings = (): void => {
-    useSettingStore.getState().updateUI({ showSettings: { open: true, tab: 0 } });
+  const handelUpdateSidebar = (): void => {
+    const sidebar = useSettingStore.getState().ui.sidebar;
+    if (!sidebar.showLeft) {
+      updateUI({ sidebar: { ...sidebar, showLeft: !sidebar.showLeft, leftWidth: constants.defaultSidebarWidth } });
+    } else {
+      updateUI({ sidebar: { ...sidebar, showLeft: !sidebar.showLeft } });
+    }
   };
 
   return (
     <Stack spacing={2} direction='row' justifyContent='flex-start'>
-      {release ? (
-        <Tooltip title={locales.new_version_available}>
-          <IconButton aria-label='settings' onClick={openSettings}>
-            <Badge variant='dot' color='warning'>
-              <CustomIcon type={'settings'} size={'m'} />
-            </Badge>
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title={locales.settings}>
-          <IconButton aria-label='settings' onClick={openSettings}>
-            <CustomIcon type={'settings'} size={'m'} />
-          </IconButton>
-        </Tooltip>
-      )}
+      <Tooltip title={locales.left_sidebar}>
+        <IconButton aria-label='sideLeft' onClick={(): void => handelUpdateSidebar()}>
+          <CustomIcon type={'sideLeft'} size={'m'} />
+        </IconButton>
+      </Tooltip>
     </Stack>
   );
 }
